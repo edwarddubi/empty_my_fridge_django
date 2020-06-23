d441921e9851107de26f5c26a6f3aa28a8c52661
maynesr <rmaynes@ufl.edu>
2020-06-23 16:04:22 -0400

    remobe % and " from database

--- cpanel/cpanel/food_network.py
+++ cpanel/cpanel/food_network.py
@@ -81,6 +81,10 @@ def parser(item, food_array):
 	item = item.replace(':', ' ')
 	item = item.replace(';', ' ')
 	item = item.replace('\'', '')
+	item = item.replace('\"', '')
+	item = item.replace('%', '')
+
+
 
 	# Breaks each word into a string array
 	split_item = item.split(" ")
