1f9bd08ed17d63b7b1d5bc45088922e08eeb3fcd
maynesr <rmaynes@ufl.edu>
2020-06-30 15:24:07 -0400

    broken

--- empty_my_fridge/empty_my_fridge/config.py
+++ empty_my_fridge/empty_my_fridge/config.py
@@ -1,6 +1,7 @@
 
 def myConfig():
     config = {
+        """
         #edward's firebase config
         "apiKey": "AIzaSyBUbHTvIh5bIb804EzPpHThsItT6i51TjY",
         "authDomain": "empty-my-fridge.firebaseapp.com",
@@ -10,9 +11,9 @@ def myConfig():
         "messagingSenderId": "394055887941",
         "appId": "1:394055887941:web:d0fa955898ec86e077872c",
         "measurementId": "G-38YRH5Z55V"
-        
-        #randolph's firebase config
         """
+        #randolph's firebase config
+        
         "apiKey": "AIzaSyBkIAGYXQ-yVBkoRqXKuBKGuXPUpNzAP7g",
         "authDomain": "empty-my-fridge-ff73c.firebaseapp.com",
         "databaseURL": "https://empty-my-fridge-ff73c.firebaseio.com",
@@ -20,8 +21,8 @@ def myConfig():
         "storageBucket": "empty-my-fridge-ff73c.appspot.com",
         "messagingSenderId": "904741248232",
         "appId": "1:904741248232:web:33ba3b5e81be538db2fd5d",
-        "measurementId": "G-9SLJ34LP0M
-        """
+        "measurementId": "G-9SLJ34LP0M"
+        
         
         #becca's firebase config
         """
@@ -32,7 +33,7 @@ def myConfig():
         'storageBucket': "empty-my-fridge-6197d.appspot.com",
         'messagingSenderId': "210802633597",
         'appId': "1:210802633597:web:239240de47cbf5e2b6ba14",
-        'measurementId': "G-26T7SBKJCX",
+        'measurementId': "G-26T7SBKJCX"
         """
     }
     return config
