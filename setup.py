4c5f532ab82aacc8ed8b15b2e7cff91f0bd58c24
edwarddubi <59847429+edwarddubi@users.noreply.github.com>
2020-07-31 02:24:08 -0400

    remove redundant comments in code

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
-  
 import os
 import setuptools
-#os.system("pip install Django==3.0.7")
-#os.system("pip install pyrebase")
-#os.system("pip install beautifulsoup4")
-#os.chdir("cpanel")
-#os.system("python manage.py runserver")
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
