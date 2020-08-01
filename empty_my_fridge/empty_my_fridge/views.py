ffd2b17be1a571ba2b893b9580e98ca1ae96a957
maynesr <rmaynes@ufl.edu>
2020-08-01 19:21:55 -0400

    DONE!!

--- empty_my_fridge/empty_my_fridge/views.py
+++ empty_my_fridge/empty_my_fridge/views.py
@@ -1757,12 +1757,14 @@ def fridge(request):
             db.child("users").child(uid).child("Fridge").child(food).remove()
 
     if search_aing:
-        all_ingredients = [i for i in all_ingredients if search_aing in i]
+        search_aing.lower()
+        all_ingredients = [i for i in all_ingredients if search_aing.lower() in i.lower()]
         if not all_ingredients:
             all_ingredients = []
     if search_fing:
+        search_fing.lower()
         if fridge_ingredients:
-            fridge_ingredients = [i for i in fridge_ingredients if search_fing in i]
+            fridge_ingredients = [i for i in fridge_ingredients if search_fing.lower() in i.lower()]
             if not fridge_ingredients:
                 fridge_ingredients = []
         else:
